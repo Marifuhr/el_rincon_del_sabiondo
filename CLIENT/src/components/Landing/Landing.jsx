dea757f496b1037b0e5b6f9404036038b9f5c7fe
Armando Martinez <alemar.martinez16@gmail.com>
2023-06-17 18:27:28 -0300

    Creacion del componente Landig.jsx y module.css

---
+++ CLIENT/src/components/Landing/Landing.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import styles from "./Landing.module.css";
+
+function Landing() {
+  return (
+    <div className={styles.landing}>
+      <h1>Welcome to my app!</h1>
+      <p>This is the landing page.</p>
+    </div>
+  );
+}
+
+export default Landing;
