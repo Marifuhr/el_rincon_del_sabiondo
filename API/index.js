903318de68910a36a7070cdc17238e746578d0ed
daprionil <davidarenaoflol23@gmail.com>
2023-06-19 22:03:01 -0500

    Codigo Base API - conexión base de datos

---
+++ API/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const morgan = require('morgan');
+const express = require('express');
+const cors = require('cors');
+
+
+//Info local enviroments
+const port = process.env.PORT_SERVER || 3001;
+const host = process.env.HOST_SERVER || 'localhost';
+
+//Local Changes
+const server = require('./src/app.js');
+const {db} = require('./src/db.js');
+
+server.use(morgan('dev'));
+server.use(express.json());
+server.use(cors());
+
+//! Añadir rutas
+//server.use(rootRouter)
+
+//sincronizando base de datos con la DB_CLOUD
+db.sync({force:true}).then(() => {
+    //execute server
+    server.listen(port, host, () => {
+        console.log(`servidor corriendo en: ${port}`);
+    })
+});
---
+++ API/src/db.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const {
+    DB_HOST_CLOUD,
+    DB_PASSWORD_CLOUD,
+    DB_USER_CLOUD,
+    DB_NAME_CLOUD,
+    DB_PORT_CLOUD,
+} = process.env;
+
+const gistUrlDB = `postgres://${DB_USER_CLOUD}:${DB_PASSWORD_CLOUD}@${DB_HOST_CLOUD}:${DB_PORT_CLOUD}/${DB_NAME_CLOUD}`;
+
+const db = new Sequelize(gistUrlDB,{logging:false});
+
+module.exports = {
+    db,
+    ...db.models
+}
