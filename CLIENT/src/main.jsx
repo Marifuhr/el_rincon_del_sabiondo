9e53e631937b87295801b97d38f26103abbce6e0
Marifuhr <mariarosafuhr@gmail.com>
2023-06-21 23:42:43 -0300

    Ultimando card

--- CLIENT/src/main.jsx
+++ CLIENT/src/main.jsx
@@ -1,14 +1,37 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <Provider>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>
+// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+//import { BrowserRouter } from "react-router-dom";
+//import { Provider } from "react-redux";
+
+{/*ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider>
+     <BrowserRouter> 
+      
+    <App />
+
+     </BrowserRouter>
+  </Provider>
+)*/}
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
