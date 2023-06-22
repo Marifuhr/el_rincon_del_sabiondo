08b5191f3ead2fb55206ea51c7dc7b4724295dce
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2023-06-22 00:36:30 -0300

    Implementacion de landingCarrusel

---
+++ CLIENT/src/components/LandingCarrusel/carrusel.jsx
@@ -0,0 +1,75 @@
+import Carousel from "react-bootstrap/Carousel";
+import "./carrusel.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function CarouselFadeExample() {
+  return (
+    <div style={{ width: "1200px", height: "400px" }}>
+      <Carousel variant="dark">
+        <Carousel.Item>
+          <img
+            className="d-block w-100 fluid"
+            src="src\assets\image\pila-libros.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <button>entrar</button>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="src\assets\image\Education process.jpeg"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="src\assets\image\buenos-libros-biblioteca.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h5>Third slide label</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="src\assets\image\book club.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h5>Third slide label</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="src\assets\image\libros.jpeg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h5>Third slide label</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default CarouselFadeExample;
