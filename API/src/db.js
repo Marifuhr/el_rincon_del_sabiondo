a651710f6055f0afa900c594b9f986e10e100e4c
daprionil <davidarenaoflol23@gmail.com>
2023-06-21 21:51:47 -0500

    Parseo - Orden de ejecución de Relaciones

--- API/src/db.js
+++ API/src/db.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const Sequelize = require('sequelize');
+const relationsModels = require('./models/relationsModels');
 const {
     DB_HOST_CLOUD,
     DB_PASSWORD_CLOUD,
@@ -17,9 +18,13 @@ const db = new Sequelize(gistUrlDB,{logging:false});
 //Executa todos los modelos en Modelo
 const pathModels = path.join(__dirname + '/models/');
 fs.readdirSync(pathModels).forEach(file => {
+    if(file === 'relationsModels.js') return;
     const pathFunctionModel = pathModels + file;
     require(pathFunctionModel)(db);
 });
+
+//Execute relations
+relationsModels(db);
 
 module.exports = {
     db,
