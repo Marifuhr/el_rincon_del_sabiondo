b4f03e8df41c310dcbe1042e8a30f4b93eefd61e
daprionil <davidarenaoflol23@gmail.com>
2023-06-21 20:17:54 -0500

    Se agregan más modelos al filtrado de getLibros.js

--- API/src/controllers/getLibros.js
+++ API/src/controllers/getLibros.js
@@ -1,19 +1,40 @@
-const { Book, Category, Country,Language } = require('../db');
+const {Author, Selling, Review, Book, Category, Country,Language } = require('../db');
 
 //Controller connect with database to realize sql query
 const getLibros = async () => {
     const libros = await Book.findAll({
         include: [
             {
-                model: Category
+                model: Category,
+                through:{
+                    attributes:[]
+                }
             },
             {
-                model: Country
+                model: Author,
+                through:{
+                    attributes:[]
+                }
             },
             {
-                model: Language
+                model: Country,
+                as:'countryPublicationData'
+            },
+            {
+                model: Language,
+                as:'languageBook'
+            },
+            {
+                model: Review
+            },
+            {
+                model: Selling,
+                as:"sellings"
             }
-        ]
+        ],
+        attributes:{
+            exclude:['language','countryPublication']
+        }
     });
     return libros;
 };
