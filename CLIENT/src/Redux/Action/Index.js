f8846cc55d6e3ac0df60893886334129a5e28147
alasiamd <alasiamd@gmail.com>
2023-06-25 04:28:41 -0300

    Archivos con nombres en minusculas

--- CLIENT/src/Redux/Action/Index.js
+++ CLIENT/src/Redux/Action/Index.js
@@ -1,25 +1,31 @@
+// import {
+//   GET_ALL_BOOKS,
+//   // GET_DETAIL_BOOKS,
+//   // FILTER_BY_CATEGORY,
+//   // FILTER_BY_PRICE,
+//   // FILTER_BY_AUTOR,
+//   // SEARCH_NAME_BOOK,
+// } from "./actions.types";
+// import axios from "axios";
 import {
-    GET_ALL_BOOKS,
-    GET_DETAIL_BOOKS,
-    FILTER_BY_CATEGORY,
-    FILTER_BY_PRICE,
-    FILTER_BY_AUTOR,
-    SEARCH_NAME_BOOK
-} from './Actions.types';
-import axios from 'axios';
+  GET_ALL_BOOKS,
+  GET_DETAIL_BOOKS,
+  FILTER_BY_CATEGORY,
+  FILTER_BY_PRICE,
+  FILTER_BY_AUTOR,
+  SEARCH_NAME_BOOK,
+} from "./actions.types";
 
-const endpoint = 'http://localhost:3001';
+const endpoint = "http://localhost:3001";
 
 export const getAllBooks = () => {
-    return async function (dispatch) {
-        try {
-            const json = await axios.get(`${endpoint}/books`)
-            return dispatch({
-                type: GET_ALL_BOOKS,
-                payload: json
-        })
-        } catch (error) {
-            
-        }
-    }
-}
+  return async function (dispatch) {
+    try {
+      const json = await axios.get(`${endpoint}/books`);
+      return dispatch({
+        type: GET_ALL_BOOKS,
+        payload: json,
+      });
+    } catch (error) {}
+  };
+};
