6fdc0770810ed447bbbbe553e43712608e34f731
daprionil <davidarenaoflol23@gmail.com>
2023-06-25 19:08:02 -0500

    Agregado Filtrado por - [category, author] - ruta /books?

--- API/src/controllers/getFilterBooksController.js
+++ API/src/controllers/getFilterBooksController.js
@@ -1,11 +1,11 @@
 const { Op } = require('sequelize');
-const { Book } = require('../db');
+const { Author, Language, Review, Selling, Book, Category } = require('../db');
 
 async function getFilterBooksController(dataFilter){
     // Campos para validar
     const fieldToValidate = {
         limit:true, title:true, description:true,language:true,
-        category:true, publisher:true
+        category:true, publisher:true, author:true
     };
     const objectValidators = {
         title: (val) => ({
@@ -22,13 +22,14 @@ async function getFilterBooksController(dataFilter){
         })
     };
 
-    //Falta categorías
-
+    //Filtra queries que no tengan valores
     const dataToFiltered = Object.entries(dataFilter);
-    const objDataFiltered = dataToFiltered.filter(([key]) => (
+    const filteredQueriesAttributes = Object.fromEntries(dataToFiltered.filter(([key]) => (
         fieldToValidate[key]
-    ))
-    .reduce((init, [key, value]) => {
+    )));
+    
+    //Entrega un objeto con las funciones validadoras para cada query
+    const objDataFiltered = Object.entries(filteredQueriesAttributes).reduce((init, [key, value]) => {
         const objectValidator = objectValidators[key];
         if(objectValidator){
             const objOp = objectValidator(value);
@@ -37,15 +38,64 @@ async function getFilterBooksController(dataFilter){
                 [key]:objOp
             };
         };
-        return {...init, [key]:value}
+        return init;
     },{});
 
     const {limit = null, ...restObjValidators} = objDataFiltered;
-    console.log(restObjValidators);
     // Consulta el modelo
-    const filteredBooks = await Book.findAll({
-        where:restObjValidators
+    const filteredBooksDB = await Book.findAll({
+        where:{
+            ...restObjValidators
+        },
+        include: [
+            {
+                model: Category,
+                through:{
+                    attributes:[]
+                }
+            },
+            {
+                model: Author,
+                through:{
+                    attributes:[]
+                }
+            },
+            {
+                model: Language,
+                as:'languageBook',
+                attributes: ['language'],
+            },
+            {
+                model: Review
+            },
+            {
+                model: Selling,
+                as:"sellings"
+            }
+        ]
     });
+
+    console.log(filteredQueriesAttributes);
+    const filteredBooks = filteredBooksDB.filter(({Categories}) => {
+        if(filteredQueriesAttributes.category){
+            const {category} = filteredQueriesAttributes;
+            const validate = Categories.some(({name}) => {
+                return new RegExp(category,'i').test(name)
+            });
+
+            return validate;
+        }
+        return true;
+    }).filter(({Authors}) => {
+        if(filteredQueriesAttributes.author){
+            const {author} = filteredQueriesAttributes;
+            const validate = Authors.some(({name}) => {
+                return new RegExp(author,'i').test(name)
+            });
+            return validate;
+        }
+        return true;
+    })
 
     return filteredBooks;
 };
--- API/src/handlers/routeBooksHandler.js
+++ API/src/handlers/routeBooksHandler.js
@@ -14,35 +14,23 @@ const getLibros = require("../controllers/getLibros.js");
 // module.exports = routeBooksHandler;
 async function routeBooksHandler(req, res) {
     try {
-<<<<<<< HEAD
-        let libros;
+        let books;
         
         //Filter query params without value
         const filterParams = Object.entries(req.query)
-                            .filter(([key,value]) => value);
+                            .filter(([,value]) => value);
         //Si existen campos válidos
         if(filterParams.length){
-            libros = await getFilterBooksController(Object.fromEntries(filterParams));
-            console.log(libros);
-            
-            res.json({libros:'perfecto'});
+            books = await getFilterBooksController(Object.fromEntries(filterParams));
+            res.json({books});
             return;
         };
-=======
-        // Este es el controlador
-        const books = await getLibros();
->>>>>>> develop
 
         // Este es el controlador /books without queries
-        libros = await getLibros();
+        books = await getLibros();
         // Desactivar respuesta 304
         res.set('Cache-Control', 'no-store');
-<<<<<<< HEAD
-        res.status(200).json({ libros });
-=======
-
         res.status(200).json({ books });
->>>>>>> develop
     } catch ({ message }) {
         res.status(500).json({ error: `Error al obtener los libros: ${message}` });
     }
