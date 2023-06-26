f3e56eda554f819f8e2ff9e137bc54abac147c47
Marifuhr <mariarosafuhr@gmail.com>
2023-06-26 16:59:26 -0300

    Finalizando Pagination

--- CLIENT/src/components/Login/Login.jsx
+++ CLIENT/src/components/Login/Login.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import styles from './Login.module.css';
 
-export default function () {
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Evita que el formulario se envíe
+    // Aquí puedes realizar las acciones necesarias, como validar los campos, enviar datos al servidor, etc.
+  };
+
   return (
-    <div>
-      <h1>Hola</h1>
+    <div className={styles.myCustomClass}>
+      <Form onSubmit={handleSubmit}>
+        <section className="vh-100 gradient-custom">
+          <div className="container py-5 h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div className="card bg-dark text-white" style={{ borderRadius: '1rem' }}>
+                  <div className="card-body p-5 text-center">
+
+                    <div className="mb-md-5 mt-md-4 pb-5">
+                      <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                      <p className="text-white-50 mb-5">Please enter your login and password!</p>
+
+                      <Form.Group controlId="formEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+
+                      <Form.Group controlId="formPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter password" />
+                      </Form.Group>
+
+                      <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
+
+                      <Button variant="outline-light" size="lg" className="px-5" type="submit">Login</Button>
+
+                      <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                        <a href="#!" className="text-white"><i className="fab fa-facebook-f fa-lg"></i></a>
+                        <a href="#!" className="text-white"><i className="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                        <a href="#!" className="text-white"><i className="fab fa-google fa-lg"></i></a>
+                      </div>
+
+                    </div>
+
+                    <div>
+                      <p className="mb-0">Don't have an account? <Link to="/signup" className="text-white-50 fw-bold">Sign Up</Link></p>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Form>
     </div>
-  )
+  );
 }
