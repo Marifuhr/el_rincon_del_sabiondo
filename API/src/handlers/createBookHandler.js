8b7476ee160cb8e8bb31574934d69d3182444489
maurocmg <maurocmg@gmail.com>
2023-06-27 19:39:59 -0300

    fixed

--- API/src/handlers/createBookHandler.js
+++ API/src/handlers/createBookHandler.js
@@ -5,6 +5,9 @@ async function createBookHandler (req, res) {
   try {
     const { title, description, language, datePublication, publisher, numberPages, rate, category, authors, image, ISBN } = req.body;
     const newBook = await createBook({ title, description, language, datePublication, publisher, numberPages, rate, category, authors, image, ISBN });
+    if (newBook.error) {
+      return res.status(418).json({ error: newBook.error });
+    }
     res.status(200).json( newBook );
   } catch (error) {
     console.error('Error al crear los libros:', error);
