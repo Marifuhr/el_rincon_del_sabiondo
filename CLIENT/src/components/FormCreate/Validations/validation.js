14e78a89bffb467501f86bbc23b7755007189e93
daprionil <davidarenaoflol23@gmail.com>
2023-06-30 00:14:43 -0500

    Refactor validator

--- CLIENT/src/components/FormCreate/Validations/validation.js
+++ CLIENT/src/components/FormCreate/Validations/validation.js
@@ -1,43 +1,44 @@
 const dictionaryNamesToLanguage = {
-  title: "Titulo",
-  authors: "autor",
-  image: "imagen",
-  language: "lenguaje",
-  numberPages: "Número de páginas",
-  description: "Descripción",
-  datePublication: "Fecha de Publicación",
-  publisher: " Editorials",
-  category: "Categoría",
-  isbn: "Código ISBN",
-  price: "Precio",
+  title: "El titulo",
+  authors: "El autor",
+  image: "La imagen",
+  language: "El lenguaje",
+  numberPages: "El número de páginas",
+  description: "La descripción",
+  datePublication: "La fecha de publicación",
+  publisher: "El editorial",
+  category: "La categoría",
+  isbn: "El código ISBN",
+  price: "El precio",
 }
 
 const dictionaryErrorsInput = {
   title: "El título no debe entre 3 y 30 caracteres",
-  isbn: `El ${dictionaryNamesToLanguage.isbn} debe tener entre 10 y 20 caracteres`
+  isbn: `${dictionaryNamesToLanguage.isbn} debe tener entre 10 y 20 caracteres`
 };
 
 const validationsInput = {
   title: val => val.length > 3 && val.length <= 30 ,
   isbn: val => val.length > 10 && val.length <= 20,
-}
+};
 
 const validate = (book) => {
+  const entriesBookValues = Object.entries(book);
   let error = "";
-  
-  for(let [key,val] in Object.entries(book)){
+
+  for(let [key,val] of entriesBookValues){
     if(!val){
-      error = `El ${dictionaryNamesToLanguage(key)} es requerido`;
+      error = `Debes completar ${dictionaryNamesToLanguage[key]}`;
       break;
     }
   };
 
   if(!error){
-    for(let [key,val] in Object.entries(book)){
+    for(let [key,val] of entriesBookValues){
       const validationInput = validationsInput[key];
       if(!validationInput) continue;
-      
-      const valueValidate = validationInput[key](val);
+
+      const valueValidate = validationInput(val);
       if(!valueValidate){
         error = dictionaryErrorsInput[key];
         break;
