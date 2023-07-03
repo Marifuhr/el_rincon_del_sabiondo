60e6280e71c6919d84f91c99fc11b48fe02d914a
daprionil <davidarenaoflol23@gmail.com>
2023-07-03 01:53:49 -0500

    Integración Ruta /logout rodri_logout

--- API/index.js
+++ API/index.js
@@ -30,15 +30,6 @@ server.use(
 server.use(morgan('dev'));
 server.use(express.json());
 server.use(cors());
-server.use(
-    auth({
-        authRequired: false,
-        issuerBaseURL: 'https://YOUR_AUTH0_DOMAIN',
-        baseURL: 'http://localhost:5173',
-        clientID: '0VbI0VgjMwnZ0YhQZZ4tJSjfcc7v7jaq',
-        secret: '-OVdWrqmPDOF-0SPFpwybF1QTfe3mUOSuHW3Xqn2SitSPLRK0zpLI_1K-Wn41mrL',
-    })
-);
 
 //! Añadir rutas
 server.use(rootRouter);
