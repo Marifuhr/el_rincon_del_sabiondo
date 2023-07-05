0d0885244ba049e2e608974bb9e660dd29f7dc68
alasiamd <alasiamd@gmail.com>
2023-07-05 10:34:49 -0300

    Ruta para envio de mail

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const routerBook = require("./routerBook");
-const routerCategory = require('./routerCategory');
+const routerCategory = require("./routerCategory");
 const routerLanguage = require("./routerLanguage");
 const routerAuthor = require("./routerAuthor");
-const routerBulkCreate = require ('./routerBulkCreate')
-const logoutRouter = require('./routerLogout');
-const routerMercadoPago = require('./routerMercadoPago');
+const routerBulkCreate = require("./routerBulkCreate");
+const logoutRouter = require("./routerLogout");
+const routerMercadoPago = require("./routerMercadoPago");
+const routeMail = require("./routeMail");
 
-const bulkCreateBooksHandler = require('../handlers/BulkCreateBooksHandler');
+const bulkCreateBooksHandler = require("../handlers/BulkCreateBooksHandler");
 
 const rootRouter = express.Router();
 //Routes and middlewares
 rootRouter.use("/books", routerBook);
-rootRouter.use('/category', routerCategory);
-rootRouter.use('/language', routerLanguage);
-rootRouter.use('/authors', routerAuthor);
-rootRouter.use('/bulkcreate', routerBulkCreate);
-rootRouter.use('/logout', logoutRouter);
-rootRouter.use('/', routerMercadoPago);
-
+rootRouter.use("/category", routerCategory);
+rootRouter.use("/language", routerLanguage);
+rootRouter.use("/authors", routerAuthor);
+rootRouter.use("/bulkcreate", routerBulkCreate);
+rootRouter.use("/logout", logoutRouter);
+rootRouter.use("/", routerMercadoPago);
+rootRouter.use("/mail", routeMail);
 
 module.exports = rootRouter;
