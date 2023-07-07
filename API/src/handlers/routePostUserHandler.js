013654d95918727e505edf96c8d71775ff5dac96
daprionil <davidarenaoflol23@gmail.com>
2023-07-06 20:57:35 -0500

    Creación de handler con validación de campos

---
+++ API/src/handlers/routePostUserHandler.js
@@ -0,0 +1,21 @@
+const findCreateUser = require("../controllers/findCreateUser");
+
+module.exports = async function(req,res) {
+    try {
+        const { name, email, picture } = req.body;
+        
+        //If values are empty or undefined
+        const validateValues = Object.values({ name, email, picture }).some((val) => !val);
+        
+        //Display error after validation
+        if(validateValues){//true
+            throw new Error('Faltan campos para la creación del usuario');
+        };
+        const dataUser = { name, email, picture };
+        const [user, created] = await findCreateUser(dataUser);
+
+        res.json({user, created});
+    } catch ({message}) {
+        res.json({error:message});
+    };
+};
