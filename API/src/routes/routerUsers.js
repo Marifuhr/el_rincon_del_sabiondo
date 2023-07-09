22873a36847ea0bb6c673d7de3a62e9145fc28a8
David Bulla <73729459+daprionil@users.noreply.github.com>
2023-07-09 12:51:51 -0500

    Creación de Ruta para Actualizar Usuario (#127)
    * Ruta PUT/users y sus validaciones
    
    * Finaliza creación de controller
    
    * fix unión con develop

--- API/src/routes/routerUsers.js
+++ API/src/routes/routerUsers.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const routePostUserHandler = require('../handlers/routePostUserHandler');
+const updateUserPutHandler = require('../handlers/updateUserPutHandler');
 
 const routerUsers = Router();
 
 routerUsers.post('/', routePostUserHandler);
+routerUsers.put('/:id_user', updateUserPutHandler);
 
 module.exports = routerUsers;
