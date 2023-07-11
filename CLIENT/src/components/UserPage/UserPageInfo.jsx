abbaeefe3b6a7afec259f8d93a77acf726a08266
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2023-07-11 15:52:15 -0300

    Renderizado de Datos de DB

--- CLIENT/src/components/UserPage/UserPageInfo.jsx
+++ CLIENT/src/components/UserPage/UserPageInfo.jsx
@@ -16,55 +16,62 @@ import {
   List,
   ListItem,
 } from "@chakra-ui/react";
-// import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
-import UserPage from "./UserPage";
+
 import { MdLocalShipping } from "react-icons/md";
-//import { useAuth0 } from "@auth0/auth0-react";
 import { useUserInfo } from "../../context/ProviderUser";
+import  formatDate  from "../../utils/formatDate";
 
 export default function Simple() {
-  const  {user}  = useUserInfo();
-  //const {name, picture, email, role, } = user;
+  const { user } = useUserInfo();
+  
 console.log(user);
   return (
     <Box>
-      {/* <UserPage /> */}
-      <Container maxW={"3xl"}>
-        <SimpleGrid
-          columns={{ base: 1, lg: 2 }}
-          spacing={{ base: 8, md: 10 }}
-          py={{ base: 18, md: 24 }}
-        >
-          <Flex>
-            <Image
-              rounded={"md"}
-              src={user.picture}
-              alt={user.name}
-              fit={"cover"}
-              align={"center"}
-              w={"100%"}
-              h={{ sm: "400px", lg: "400px" }}
-            />
-          </Flex>
-          <Stack spacing={{ base: 6, md: 10 }}>
-            <Box as={"header"}>
-              <Heading
-                lineHeight={1.1}
-                fontWeight={600}
-                fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
-              >
-                {user.name}
-              </Heading>
-              <Text
-                color={useColorModeValue("gray.900", "gray.400")}
-                fontWeight={300}
-                fontSize={"2xl"}
-              >
-                {user.role}
-              </Text>
-            </Box>
-
-            <Stack
+      {/* <Container maxW={"4xl"}> */}
+      <SimpleGrid
+        columns={{ base: 1, md: 2, lg: 2 }}
+        spacing={{ base: 2, md: 0 }}
+        py={{ base: 18, md: 24 }}
+      >
+        <Flex>
+          <Image
+            rounded={"md"}
+            src={user.picture}
+            alt={user.name}
+            fit={"cover"}
+            align={"center"}
+            w={"70%"}
+            h={{ sm: "300px", lg: "300px" }}
+          />
+        </Flex>
+        <Stack spacing={{ base: 6, md: 1 }}>
+          <Box as={"header"}>
+            <Heading
+              lineHeight={5.9}
+              fontWeight={600}
+              fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
+            >
+              {user.name}
+            </Heading>
+            {/*<Text
+              color={useColorModeValue("gray.500", "gray.400")}
+              fontSize={"2xl"}
+              fontWeight={"300"}
+            >
+              {`Id: ${user.IdUser}`}
+            </Text>
+            <Text
+              color={useColorModeValue("gray.900", "gray.400")}
+              fontWeight={300}
+              fontSize={"2xl"}
+            >
+              {user.role}
+            </Text>*/}
+          </Box>
+        </Stack>
+      </SimpleGrid>
+      {/* </Container> */}
+      {/* <Stack
               spacing={{ base: 4, sm: 6 }}
               direction={"column"}
               divider={
@@ -72,70 +79,98 @@ console.log(user);
                   borderColor={useColorModeValue("gray.200", "gray.600")}
                 />
               }
-            >
-              <VStack spacing={{ base: 4, sm: 6 }}>
-                <Text
-                  color={useColorModeValue("gray.500", "gray.400")}
-                  fontSize={"2xl"}
-                  fontWeight={"300"}
-                >
-                  {`Id: ${user.IdUser}`}
-                </Text>
-                <Text fontSize={"lg"}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
-                  aliquid amet at delectus doloribus dolorum expedita hic, ipsum
-                  maxime modi nam officiis porro, quae, quisquam quos
-                  reprehenderit velit? Natus, totam.
-                </Text>
-              </VStack>
-              <Box>
-                <Text
-                  fontSize={{ base: "16px", lg: "18px" }}
-                  color={useColorModeValue("yellow.500", "yellow.300")}
-                  fontWeight={"500"}
-                  textTransform={"uppercase"}
-                  mb={"4"}
-                >
-                  Datos
-                </Text>
+            > 
+       <VStack spacing={{ base: 4, sm: 6 }}>
+        <Text fontSize={"lg"}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid
+          amet at delectus doloribus dolorum expedita hic, ipsum maxime modi nam
+          officiis porro, quae, quisquam quos reprehenderit velit? Natus, totam.
+        </Text>
+      </VStack> */}
+      <Box>
+        <Text
+          fontSize={{ base: "16px", lg: "18px" }}
+          color={useColorModeValue("yellow.500", "yellow.300")}
+          fontWeight={"500"}
+          textTransform={"uppercase"}
+          mb={"4"}
+        >
+          Datos
+        </Text>
 
-                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-                  <List spacing={2}>
-                    <ListItem>
-                      <Text as={"span"} fontWeight={"bold"}>
-                        Email:
-                      </Text>{" "}
-                      {user.email}
-                    </ListItem>
-                    {/* <ListItem>Master Chronometer Certified</ListItem>{" "}
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+          <List spacing={2}>
+            <ListItem>
+              <Text as={"span"} fontWeight={"bold"}>
+                Rol:
+              </Text>{" "}
+              {user.role}
+            </ListItem>
+            <ListItem>
+              <Text as={"span"} fontWeight={"bold"}>
+                Id:
+              </Text>{" "}
+              {user.IdUser}
+            </ListItem>
+            <ListItem>
+              <Text as={"span"} fontWeight={"bold"}>
+                Email:
+              </Text>{" "}
+              {user.email}
+            </ListItem>
+            <ListItem>
+              <Text as={"span"} fontWeight={"bold"}>
+                Actualizacion:
+              </Text>{" "}
+              {formatDate(user.updatedAt)}
+            </ListItem>
+            {/*  <ListItem>Master Chronometer Certified</ListItem>{" "}
                     <ListItem>Tachymeter</ListItem>
                   </List>
                   <List spacing={2}>
                     <ListItem>Anti‑magnetic</ListItem>
                     <ListItem>Chronometer</ListItem>
                     <ListItem>Small seconds</ListItem>*/}
-                  </List> 
-                </SimpleGrid>
-              </Box>
-              <Box>
-                <Text
-                  fontSize={{ base: "16px", lg: "18px" }}
-                  color={useColorModeValue("yellow.500", "yellow.300")}
-                  fontWeight={"500"}
-                  textTransform={"uppercase"}
-                  mb={"4"}
-                >
-                  Reviews
-                </Text>
+          </List>
+        </SimpleGrid>
+      </Box>
+      <Box>
+        <Text
+          fontSize={{ base: "16px", lg: "18px" }}
+          color={useColorModeValue("yellow.500", "yellow.300")}
+          fontWeight={"500"}
+          textTransform={"uppercase"}
+          mb={"4"}
+        >
+          Reviews
+        </Text>
 
-                <List spacing={2}>
-                  <ListItem>
-                    <Text as={"span"} fontWeight={"bold"}>
-                      Mi comentario:
-                    </Text>{" "}
-                    {user.Review}
-                  </ListItem>
-                  {/* <ListItem>
+        <List spacing={2}>
+          <ListItem>
+            <Text as={"span"} fontWeight={"bold"}>
+              Mi comentario:
+            </Text>{" "}
+            {user.Review}
+          </ListItem>
+        </List>
+      </Box>
+      <Box>
+        <Text
+          fontSize={{ base: "16px", lg: "18px" }}
+          color={useColorModeValue("yellow.500", "yellow.300")}
+          fontWeight={"500"}
+          textTransform={"uppercase"}
+          mb={"4"}
+        >
+          Compras realizadas
+        </Text>
+
+        <List spacing={2}>
+          <ListItem>
+            <Text as={"span"} fontWeight={"bold"}>Total de Libro:</Text> {user.SellingTotals}
+          </ListItem>
+
+          {/* <ListItem>
                     <Text as={"span"} fontWeight={"bold"}>
                       Bracelet:
                     </Text>{" "}
@@ -172,11 +207,10 @@ console.log(user);
                     </Text>{" "}
                     5 bar (50 metres / 167 feet){" "}
                   </ListItem> */}
-                </List>
-              </Box>
-            </Stack>
+        </List>
+      </Box>
 
-            <Button
+      {/* <Button
               rounded={"none"}
               w={"full"}
               mt={8}
@@ -200,10 +234,8 @@ console.log(user);
             >
               <MdLocalShipping />
               <Text>2-3 business days delivery</Text>
-            </Stack>
-          </Stack>
-        </SimpleGrid>
-      </Container>
+            </Stack> */}
+      {/* </Stack> */}
     </Box>
   );
 }
