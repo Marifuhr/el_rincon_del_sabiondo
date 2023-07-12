eb016a1f9fd5c0f9e567f966dcf3b6bef691d2df
David Bulla <73729459+daprionil@users.noreply.github.com>
2023-07-11 20:38:31 -0500

    Creación sellingTotal (#140)
    * Agregando Stock a Libros
    
    * Fijando Cambios createReview.js
    
    * Añadiendo routerSellings en Indeax
    
    * Create ROUTE GET/sellings
    
    * Create route GET/sellings/:IdSelling
    
    * Create route POST/sellings - Añaden Tipos de Errores y Validaciones
    
    * Create selling individual

--- API/src/controllers/createReview.js
+++ API/src/controllers/createReview.js
@@ -2,26 +2,26 @@ const { Book, User, Review } = require('../db');
 
 module.exports = async function ({ IdUser, IdBook, rate, description }) {
   try {
-    const userExist = await User.findOne({ where: { email: IdUser } });
+    const userExist = await User.findByPk(IdUser);
     const bookExist = await Book.findByPk(IdBook);
     
-   if (userExist && bookExist) {
-     const newReview = await Review.create({
-       IdUser : userExist.IdUser,
-       IdBook,
-       rate,
-       description
-     })
-     return newReview
-   } else {
-    if (!userExist) {
-      return { error: 'User does not exist' };
+    if (userExist && bookExist) {
+      const newReview = await Review.create({
+        IdUser: userExist.IdUser,
+        IdBook,
+        rate,
+        description
+      })
+      return newReview
     } else {
-      if (!bookExist) {
-        return { error: 'Book does not exist' };
-      }      
+      if (!userExist) {
+        return { error: 'User does not exist' };
+      } else {
+        if (!bookExist) {
+          return { error: 'Book does not exist' };
+        }
+      }
     }
-   }
   } catch (error) {
     return { error: error.message };
   }
---
+++ API/src/handlers/routeCreateSellingTotalHandler.js
@@ -0,0 +1,26 @@
+const createBulKSellingIndividual = require("../controllers/createBulKSellingIndividual");
+const createSellingTotal = require("../controllers/createSellingTotal");
+const getASellingTotal = require("../controllers/getASellingTotal");
+const { checkBooksValuesOrId } = require("../utils/checkBooksValuesOrId");
+const checkEmptyValuesObject = require("../utils/checkEmptyValuesObject");
+
+module.exports = async function(req,res){
+    try {
+        const { products, IdUser } = req.body;
+        
+        //Validate values in body
+        checkEmptyValuesObject({ products, IdUser });
+        checkBooksValuesOrId(products);
+
+        const SellingTotalCreated = await createSellingTotal({IdUser});
+        const {IdSellingTotal} = SellingTotalCreated;
+        
+        await createBulKSellingIndividual({products, IdSellingTotal });
+
+        const SellingTotal = await getASellingTotal({IdSelling: IdSellingTotal});
+        res.json(SellingTotal);
+    } catch ({message, name}) {
+        console.log(name);
+        res.json({error:`${name} | ${message}`});
+    };
+};
