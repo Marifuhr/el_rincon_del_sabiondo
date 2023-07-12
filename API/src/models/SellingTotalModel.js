3f8688ef2f2c30eb18207d738701a5e13e84f8b8
maurocmg <maurocmg@gmail.com>
2023-07-12 16:18:23 -0300

    fix_Profile_Provider_reviews_Editprofile

--- API/src/models/SellingTotalModel.js
+++ API/src/models/SellingTotalModel.js
@@ -1,36 +1,6 @@
-<<<<<<< HEAD
-// const { DataTypes } = require("sequelize")
-// const generateIdUUID = require("../utils/generateIdUUID")
-
-// module.exports = function(database){
-//     database.define('SellingTotal',{
-//         IdSellingTotal:{
-//             type: DataTypes.UUID,
-//             defaultValue: DataTypes.UUIDV4,
-//             primaryKey:true,
-//             set(value){
-//                 //! VALIDATE THIS AFTER validate all
-//                 this.setDataValue('IdSellingTotal',value ? value : generateIdUUID());
-//             }
-//         },
-//         IdUser:{
-//             type: DataTypes.STRING(50),
-//             allowNull: false
-//         }
-//     },{
-//         timestamps:true,
-//         createdAt:true,
-//         updatedAt:false
-//     })
-// }
 const { DataTypes } = require("sequelize");
 const generateIdUUID = require("../utils/generateIdUUID");
 
-=======
-const { DataTypes } = require("sequelize");
-const generateIdUUID = require("../utils/generateIdUUID");
-
->>>>>>> 3c7ddb452c6c46a314e5bb3a73d3120359ddba57
 module.exports = function(database) {
     database.define('SellingTotal', {
         IdSellingTotal: {
@@ -63,8 +33,4 @@ module.exports = function(database) {
         createdAt: true,
         updatedAt: false
     });
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 3c7ddb452c6c46a314e5bb3a73d3120359ddba57
