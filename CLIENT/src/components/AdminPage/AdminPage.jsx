416be6a13c99cf8339558242e0f6773172078489
BLENDA23 <blenda.prg@gmail.com>
2023-07-14 13:40:07 -0500

    dashboard

---
+++ CLIENT/src/components/AdminPage/AdminPage.jsx
@@ -0,0 +1,211 @@
+import { Link, Outlet } from "react-router-dom";
+import logo from "../../assets/image/Logo.png";
+import { Profile } from "../Login/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import { clearStorageCart } from "../../Redux/Action/Index";
+import {
+  Box,
+  CloseButton,
+  Flex,
+  HStack,
+  Icon,
+  useColorModeValue,
+  IconButton,
+  // Link,
+  Image,
+  Drawer,
+  DrawerContent,
+  Text,
+  useDisclosure,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import {
+  FiHome,
+  FiUser,
+  FiShoppingCart,
+  FiMessageSquare,
+  FiCheckCircle,
+  FiMenu,
+  FiBell,
+} from "react-icons/fi";
+
+const LinkItems = [
+  { name: "Inicio", icon: FiHome, route: "/home" },
+  { name: "Editar Perfil", icon: FiUser, route: "/profile/myProfile" },
+  { name: "Usuarios", icon: FiShoppingCart, route: "/admin/usuariosAdmin" },
+  { name: "Agregar Libros", icon: FiMessageSquare, route: "/create" },
+  // { name: "Facturas", icon: FiCheckCircle, route: "/profile/billing" },
+];
+
+export default function SidebarWithHeader() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: "none", md: "block" }}
+      />
+      <Drawer
+        autoFocus={false}
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+      >
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      {/* mobilenav */}
+      <MobileNav onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {<Outlet />}
+      </Box>
+    </Box>
+  );
+}
+
+const SidebarContent = ({ onClose, ...rest }) => {
+  return (
+    <Box
+      transition="3s ease"
+      bg={useColorModeValue("#70a57b", "gray.900")}
+      borderRight="1px"
+      borderRightColor={useColorModeValue("gray.200", "gray.700")}
+      w={{ base: "full", md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}
+    >
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        {/* <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+          Logo
+        </Text> */}
+        <Image
+          boxSize="70px"
+          objectFit="cover"
+          src={logo}
+          alt="El Rincón del Sabiondo"
+        />
+        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
+      </Flex>
+      {LinkItems.map((link) => (
+        <NavItem key={link.name} icon={link.icon} route={link.route}>
+          {link.name}
+        </NavItem>
+      ))}
+    </Box>
+  );
+};
+
+const NavItem = ({ icon, children, route, ...rest }) => {
+  return (
+    <Link
+      href="#"
+      style={{ textDecoration: "none" }}
+      _focus={{ boxShadow: "none" }}
+      to={route}
+    >
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: "cyan.400",
+          color: "white",
+        }}
+        {...rest}
+      >
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: "white",
+            }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </Link>
+  );
+};
+
+const MobileNav = ({ onOpen, ...rest }) => {
+  const { logout } = useAuth0();
+
+  const handleLogout = () => {
+    clearStorageCart();
+    console.log("Realizando acciones antes de hacer logout...");
+
+    // Hacer logout
+    logout({ returnTo: window.location.origin });
+  };
+  return (
+    <Flex
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 4 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue("#70a57b", "gray.900")}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+      justifyContent={{ base: "space-between", md: "flex-end" }}
+      {...rest}
+    >
+      <IconButton
+        display={{ base: "flex", md: "none" }}
+        onClick={onOpen}
+        variant="outline"
+        aria-label="open menu"
+        icon={<FiMenu />}
+      />
+
+      <Text
+        display={{ base: "flex", md: "none" }}
+        fontSize="1xl"
+        fontFamily="monospace"
+        fontWeight="bold"
+      >
+        El Rincón del Sabiondo
+      </Text>
+
+      <HStack spacing={{ base: "0", md: "6" }}>
+        <IconButton
+          size="lg"
+          variant="ghost"
+          aria-label="open menu"
+          icon={<FiBell />}
+        />
+        <Flex alignItems={"center"}>
+          <Menu>
+            <Profile onClick={handleLogout} />
+            <MenuList
+              bg={useColorModeValue("white", "gray.900")}
+              borderColor={useColorModeValue("gray.200", "gray.700")}
+            >
+              {/* <MenuItem>Profile</MenuItem>
+              <MenuItem>Settings</MenuItem>*/}
+              <MenuItem as={Link} to="/profile">
+                Perfil
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem onClick={handleLogout}>Sign out</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </HStack>
+    </Flex>
+  );
+};
---
+++ CLIENT/src/components/UsuariosAdmin/UsuariosAdmin.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useUserInfo } from "../../context/ProviderUser"; 
+
+function UsuariosAdmin() {
+  const { user } = useUserInfo();
+  const customerId = user ? user.customerId : null; // Obtener el customerId del usuario del contexto
+
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    const obtenerUsuarios = async () => {
+      try {
+        const response = await axios.get(`https://ser-back-sab.onrender.com/users`);
+        setUsuarios(response.data); // Actualizar el estado con los usuarios
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error al obtener los usuarios:', error);
+      }
+    };
+    obtenerUsuarios();
+  }, []);
+
+ 
+
+  return (
+    <div>
+       <table width="800" border="1">
+  <tr>
+    <td>USUARIO</td>
+    <td>TIPO USUARIO</td>
+    <td>ESTADO</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+    </div>
+  );
+}
+
+export default UsuariosAdmin;
