12786cb61b323ac89dfbe365db91cfa0e325962b
Armando Martinez <alemar.martinez16@gmail.com>
2023-07-15 19:25:23 -0300

    modificacion del componente BotonVolver.jsx

--- CLIENT/src/elements/ButtonVolver.jsx
+++ CLIENT/src/elements/ButtonVolver.jsx
@@ -1,15 +1,31 @@
 import { Link as ChakraLink, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+
 function ButtonVolver() {
-    const navigate = useNavigate();
-    const handleClick = () => {
-        navigate(-1);
-    }
-    return (
-        <Button onClick={handleClick} as={ChakraLink} to="/home" className="btn-back">
-            Volver
-        </Button>
-    );
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(-1);
+  };
+  return (
+    <Button
+      color={"white"}
+      bg={"#70a57b"}
+      margin={"10px"}
+      onClick={handleClick}
+      as={ChakraLink}
+      to="/home"
+      className="btn-back"
+      _hover={{
+        color: "white",
+        transform: "translateY(-2px)",
+        boxShadow: "lg",
+        textDecoration: "none"
+      }}
+      
+    >
+      Volver
+    </Button>
+  );
 }
 
 export default ButtonVolver;
