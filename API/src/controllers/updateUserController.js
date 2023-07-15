b9eefa8f6e7154249ed2a7f5a6e4c3fb3f0c4a7e
alasiamd <alasiamd@gmail.com>
2023-07-15 20:46:54 -0300

    Se resuelve error del campo isActive

--- API/src/controllers/updateUserController.js
+++ API/src/controllers/updateUserController.js
@@ -1,80 +1,82 @@
-const { User } = require('../db.js');
+const { User } = require("../db.js");
 
 const errorFieldsToUpdate = {
-    role: 'El role no es válido',
-    picture: 'El url de la imágen no es válida',
-    name: 'El nombre debe tener entre 5 - 50 carácteres',
-    isActive: 'El estado de Usuario debe ser un valor Booleano',
-    email: 'El email no es válido',
-    address: 'La dirección no es válida',
-    province: 'La provincia no es válida',
-    postalCode: 'El código postal no es válido',
-    country: 'El país no es válido',
-    city: 'La ciudad no es válida'
+  role: "El role no es válido",
+  picture: "El url de la imágen no es válida",
+  name: "El nombre debe tener entre 5 - 50 carácteres",
+  // isActive: 'El estado de Usuario debe ser un valor Booleano',
+  email: "El email no es válido",
+  address: "La dirección no es válida",
+  province: "La provincia no es válida",
+  postalCode: "El código postal no es válido",
+  country: "El país no es válido",
+  city: "La ciudad no es válida",
 };
 
 const validationsAttributes = {
-    role: rol => {
-        const roles = ['user', 'moderator', 'admin', 'superuser'];
-        return roles.includes(rol);
-    },
-    picture: (image) => {
-        const erValidateUrl = /^(https?:\/\/)?(([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(#[-a-z\d_]*)?$/i;
-	    return !!erValidateUrl.test(image);
-    },
-    name: nameString => {
-        const l = nameString.length;
-        return l >= 5 && l <= 50
-    },
-    isActive: stateUser => [true,false].includes(stateUser),
-    email: emailToValidate => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(emailToValidate),
-    address: addressString => {
-        const l = addressString.length;
-        return l >= 5 && l <= 100
-    },
-    province: provinceString =>{
-        const l = provinceString.length;
-        return l >= 5 && l <= 50
-    },
-    postalCode: postalCodeString => {
-        const l = postalCodeString.toString().length;
-        return l >= 4 && l <= 10
-    },
-    country: countryString => {
-        const l = countryString.length;
-        return l >= 4 && l <= 50
-    },
-    city: cityString => {
-        const l = cityString.length;
-        return l >= 5 && l <= 50
-    }
-
+  role: (rol) => {
+    const roles = ["user", "moderator", "admin", "superuser"];
+    return roles.includes(rol);
+  },
+  picture: (image) => {
+    const erValidateUrl =
+      /^(https?:\/\/)?(([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(#[-a-z\d_]*)?$/i;
+    return !!erValidateUrl.test(image);
+  },
+  name: (nameString) => {
+    const l = nameString.length;
+    return l >= 5 && l <= 50;
+  },
+  // isActive: stateUser => [true,false].includes(stateUser),
+  email: (emailToValidate) =>
+    /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(emailToValidate),
+  address: (addressString) => {
+    const l = addressString.length;
+    return l >= 5 && l <= 100;
+  },
+  province: (provinceString) => {
+    const l = provinceString.length;
+    return l >= 5 && l <= 50;
+  },
+  postalCode: (postalCodeString) => {
+    const l = postalCodeString.toString().length;
+    return l >= 4 && l <= 10;
+  },
+  country: (countryString) => {
+    const l = countryString.length;
+    return l >= 4 && l <= 50;
+  },
+  city: (cityString) => {
+    const l = cityString.length;
+    return l >= 5 && l <= 50;
+  },
 };
 
-module.exports = async function({id_user, dataToUpdate}){
-    //Validate query values
-    for(let [fieldKey,fieldValue] of Object.entries(dataToUpdate)){
-        //Get function to field validate
-        const validationForField = validationsAttributes[fieldKey]
-        if(!validationForField) continue;
+module.exports = async function ({ id_user, dataToUpdate }) {
+  //Validate query values
+  for (let [fieldKey, fieldValue] of Object.entries(dataToUpdate)) {
+    //Get function to field validate
+    const validationForField = validationsAttributes[fieldKey];
+    if (!validationForField) continue;
 
-        //Execute function to field validate
-        const validateValueField = validationForField(fieldValue);
-        if(!validateValueField){
-            throw new Error(errorFieldsToUpdate[fieldKey]);
-        };
+    //Execute function to field validate
+    const validateValueField = validationForField(fieldValue);
+    if (!validateValueField) {
+      throw new Error(errorFieldsToUpdate[fieldKey]);
     }
+  }
 
-    //At this point the values have no errors
-    const findUser = await User.findOne({
-        where:{ IdUser: id_user}
-    });
-    
-    //Si el usuario no existe
-    if(!findUser) throw new Error(`El usuario que intentas actualizar no existe`);
+  //At this point the values have no errors
+  const findUser = await User.findOne({
+    where: { IdUser: id_user },
+  });
 
-    //Si ya existe lo actualiza
-    const user = await findUser.update(dataToUpdate);
+  //Si el usuario no existe
+  if (!findUser)
+    throw new Error(`El usuario que intentas actualizar no existe`);
 
-    return user;
+  //Si ya existe lo actualiza
+  const user = await findUser.update(dataToUpdate);
+
+  return user;
 };
