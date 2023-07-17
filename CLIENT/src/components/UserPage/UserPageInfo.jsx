7cb82333502a689b8019ce58662721d81f9dc0c8
MariFuhr <mariarosafuhr@gmail.com>
2023-07-16 21:07:51 -0300

    listo ordenamiento alfabetico de usuarios en admin

--- CLIENT/src/components/UserPage/UserPageInfo.jsx
+++ CLIENT/src/components/UserPage/UserPageInfo.jsx
@@ -67,35 +67,6 @@ export default function Simple() {
         </Stack>
       </SimpleGrid>
       <Box>
-        {/* <Container maxW={"4xl"}> */}
-        <SimpleGrid
-          columns={{ base: 1, md: 2, lg: 2 }}
-          spacing={{ base: 2, md: 0 }}
-          py={{ base: 18, md: 24 }}
-        >
-          <Flex>
-            <Image
-              rounded={"md"}
-              src={user.picture}
-              alt={user.name}
-              fit={"cover"}
-              align={"center"}
-              w={"70%"}
-              h={{ sm: "300px", lg: "300px" }}
-            />
-          </Flex>
-          <Stack spacing={{ base: 6, md: 1 }}>
-            <Box as={"header"}>
-              <Heading
-                lineHeight={5.9}
-                fontWeight={600}
-                fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
-              >
-                {user.name}
-              </Heading>
-            </Box>
-          </Stack>
-        </SimpleGrid>
         <Box>
           <Text
             fontSize={{ base: "16px", lg: "18px" }}
@@ -137,39 +108,7 @@ export default function Simple() {
           </SimpleGrid>
         </Box>
         <Box>
-          <Text
-            fontSize={{ base: "16px", lg: "18px" }}
-            color="yellow.500"
-            fontWeight={"500"}
-            textTransform={"uppercase"}
-            mb={"4"}
-          >
-            Reviews
-          </Text>
-
           <List spacing={2}>
-            <ListItem>
-              <Text as={"span"} fontWeight={"bold"}>
-                Mi comentario:
-              </Text>{" "}
-              {user.Review}
-            </ListItem>
-          </List>
-        </Box>
-        <Box>
-          <Text
-            fontSize={{ base: "16px", lg: "18px" }}
-            color="yellow.500"
-            fontWeight={"500"}
-            textTransform={"uppercase"}
-            mb={"4"}
-          >
-            Compras realizadas
-          </Text>
-          <List spacing={2}>
-            <ListItem>
-              <Text as={"span"} fontWeight={"bold"}>Total de Libro:</Text> {user.SellingTotals}
-            </ListItem>
             <ListItem>
               <Text as={"span"} fontWeight={"bold"}>
                 País:
@@ -195,7 +134,7 @@ export default function Simple() {
               {user.postalCode}
             </ListItem>
           </List>
-        </SimpleGrid>
+        </Box>
       </Box>
       <Box>
         <Text
