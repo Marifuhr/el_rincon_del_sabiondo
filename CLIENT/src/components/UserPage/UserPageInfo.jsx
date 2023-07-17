ba373a347c8a2c73b4ff0649226715db6bc881cc
rodri72 <70957624+rodri72@users.noreply.github.com>
2023-07-17 00:47:34 -0300

    tarea lista

--- CLIENT/src/components/UserPage/UserPageInfo.jsx
+++ CLIENT/src/components/UserPage/UserPageInfo.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Container,
@@ -12,12 +13,12 @@ import {
   StackDivider,
   ListItem,
   List,
-  useColorModeValue
+  useColorModeValue,
+  Input,
 } from "@chakra-ui/react";
 import { StarIcon } from "@chakra-ui/icons";
 import { useUserInfo } from "../../context/ProviderUser";
 import formatDate from "../../utils/formatDate";
-import { useState, useEffect } from "react";
 
 export default function Simple() {
   const { user } = useUserInfo();
@@ -26,21 +27,22 @@ export default function Simple() {
   const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
-    setReviews(user.Reviews);
+    setReviews(user.Reviews || []);
   }, [user.Reviews]);
 
   useEffect(() => {
-    setShopping(user.SellingTotals);
-    setTotalPrice(user.SellingTotals.reduce((init, {products}) => {
-      const price = eval(products.map(({Book}) => Book.price).toString().replaceAll(',',"+"));
-      return (init + price);
-    },0).toFixed(2));
+    setShopping(user.SellingTotals || []);
+    setTotalPrice(
+      user.SellingTotals?.reduce((total, book) => total + book.price, 0) || 0
+    );
   }, [user.SellingTotals]);
 
+  const handleChange = (field, value) => {
+    // Aquí puedes manejar la lógica para actualizar el campo correspondiente en el estado
+  };
+
   return (
-<<<<<<< HEAD
     <Box>
-      {/* <Container maxW={"4xl"}> */}
       <SimpleGrid
         columns={{ base: 1, md: 2, lg: 2 }}
         spacing={{ base: 2, md: 0 }}
@@ -70,9 +72,6 @@ export default function Simple() {
         </Stack>
       </SimpleGrid>
 
-=======
-    
->>>>>>> 013c696e6d0c08c79393b06a333c0b5bc42cce3b
       <Box>
         <Text
           fontSize={{ base: "16px", lg: "18px" }}
@@ -83,93 +82,6 @@ export default function Simple() {
         >
           Datos
         </Text>
-          <Flex>
-            <Image
-              rounded={"md"}
-              src={user.picture}
-              alt={user.name}
-              fit={"cover"}
-              align={"center"}
-              w={"70%"}
-              h={{ sm: "300px", lg: "300px" }}
-            />
-          </Flex>
-          <Stack spacing={{ base: 6, md: 1 }}>
-            <Box as={"header"}>
-              <Heading
-                lineHeight={5.9}
-                fontWeight={600}
-                fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
-              >
-                {user.name}
-              </Heading>
-            </Box>
-          </Stack>
-        </SimpleGrid>
-
-        <Box>
-          <Text
-            fontSize={{ base: "16px", lg: "18px" }}
-            color="yellow.500"
-            fontWeight={"500"}
-            textTransform={"uppercase"}
-            mb={"4"}
-          >
-            Datos
-          </Text>
-
-          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-            <List spacing={2}>
-              <ListItem>
-                <Text as={"span"} fontWeight={"bold"}>
-                  Rol:
-                </Text>{" "}
-                {user.role}
-              </ListItem>
-              <ListItem>
-                <Text as={"span"} fontWeight={"bold"}>
-                  Id:
-                </Text>{" "}
-                {user.IdUser}
-              </ListItem>
-              <ListItem>
-                <Text as={"span"} fontWeight={"bold"}>
-                  Email:
-                </Text>{" "}
-                {user.email}
-              </ListItem>
-              <ListItem>
-                <Text as={"span"} fontWeight={"bold"}>
-                  Actualizacion:
-                </Text>{" "}
-                {formatDate(user.updatedAt)}
-              </ListItem>
-            </List>
-          </SimpleGrid>
-        </Box>
-        <Box>
-            <ListItem>
-              <Text as={"span"} fontWeight={"bold"}>
-                País:
-              </Text>{" "}
-              {user.country}
-            </ListItem>
-            <ListItem>
-              <Text as={"span"} fontWeight={"bold"}>
-                Provincia:
-              </Text>{" "}
-              {user.province}
-            </ListItem>
-
-          <Text
-            fontSize={{ base: "16px", lg: "18px" }}
-            color={useColorModeValue("yellow.500", "yellow.300")}
-            fontWeight={"500"}
-            textTransform={"uppercase"}
-            mb={"4"}
-          >
-            Reviews
-          </Text>
 
         <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
           <List spacing={2}>
@@ -193,7 +105,7 @@ export default function Simple() {
             </ListItem>
             <ListItem>
               <Text as={"span"} fontWeight={"bold"}>
-                Actualizacion:
+                Actualización:
               </Text>{" "}
               {formatDate(user.updatedAt)}
             </ListItem>
@@ -217,13 +129,14 @@ export default function Simple() {
             </ListItem>
             <ListItem>
               <Text as={"span"} fontWeight={"bold"}>
-                Codigo Postal:
+                Código Postal:
               </Text>{" "}
               {user.postalCode}
             </ListItem>
           </List>
         </SimpleGrid>
       </Box>
+
       <Box>
         <Text
           fontSize={{ base: "16px", lg: "18px" }}
@@ -236,27 +149,25 @@ export default function Simple() {
         </Text>
 
         <List spacing={2}>
-          <ListItem>
-            <Text as={"span"} fontWeight={"bold"}></Text>{" "}
-            {reviews?.slice(0, 3).map((review) => (
+          {reviews?.slice(0, 3).map((review) => (
+            <ListItem key={review.IdBook}>
               <Box>
-                <ListItem>
-                  <Image
-                    rounded={"md"}
-                    src={review.Book.image}
-                    alt={review.Book.title}
-                    fit={"cover"}
-                    align={"center"}
-                    w={"10%"}
-                    h={{ sm: "100px", lg: "70px" }}
-                  />
-<<<<<<< HEAD
-                </ListItem>
+                <Image
+                  rounded={"md"}
+                  src={review.Book?.image}
+                  alt={review.Book?.title}
+                  fit={"cover"}
+                  align={"center"}
+                  w={"10%"}
+                  h={{ sm: "100px", lg: "70px" }}
+                />
+              </Box>
+              <Box>
                 <ListItem>
                   <Text as={"span"} fontWeight={"bold"}>
                     Nombre del Libro:
                   </Text>{" "}
-                  {review.Book.title}
+                  {review.Book?.title}
                 </ListItem>
                 <ListItem>
                   <Text as={"span"} fontWeight={"bold"}>
@@ -281,10 +192,11 @@ export default function Simple() {
                 </ListItem>
                 <br />
               </Box>
-            ))}
-          </ListItem>
+            </ListItem>
+          ))}
         </List>
       </Box>
+
       <Box>
         <Text
           fontSize={{ base: "16px", lg: "18px" }}
@@ -302,12 +214,12 @@ export default function Simple() {
               Total de Libro: {totalPrice}
             </Text>{" "}
             {shopping?.slice(0, 3).map((book) => (
-              <Box>
+              <Box key={book.IdSellingTotal}>
                 <ListItem>
                   <Text as={"span"} fontWeight={"bold"}>
                     Producto:
                   </Text>{" "}
-                  {book.product}
+                  {book.BookTitle}
                 </ListItem>
                 <ListItem>
                   <Text as={"span"} fontWeight={"bold"}>
@@ -319,77 +231,7 @@ export default function Simple() {
             ))}
           </ListItem>
         </List>
-=======
-                  <ListItem display="flex" flexDirection="column">
-                    <Text as={"span"} fontWeight={"bold"}>
-                      Nombre del Libro:
-                      <span style={{fontWeight:"normal"}}> {review.Book.title}</span>
-                    </Text>
-                    <Text as={"span"} fontWeight={"bold"}>
-                      Id del Libro:
-                      <span style={{fontWeight:"normal"}}> {review.IdBook}</span>
-                    </Text>
-                    <Text as={"span"} fontWeight={"bold"}>
-                      Descripción:
-                      <span style={{fontWeight:"normal"}}> {review.description}</span>
-                    </Text>
-                    <Box>
-                      {[1, 2, 3, 4, 5].map((value) => (
-                        <StarIcon
-                          key={value}
-                          boxSize={3}
-                          color={value <= review.rate ? "yellow.500" : "gray.300"}
-                        />
-                      ))}
-                    </Box>
-                  </ListItem>
-                </Box>
-              ))}
-            </ListItem>
-          </List>
-        </Box>
-        <Box>
-          <Text
-            fontSize={{ base: "16px", lg: "18px" }}
-            color={useColorModeValue("yellow.500", "yellow.300")}
-            fontWeight={"500"}
-            textTransform={"uppercase"}
-            mb={"4"}
-          >
-            Compras realizadas
-          </Text>
-<Box>
-          <List spacing={2}>
-            <ListItem>
-              <Text as={"span"} fontWeight={"bold"}>
-                Total de Libro: {totalPrice}
-              </Text>{" "}
-              {shopping?.slice(0, 3).map((book, i) => (
-                <Box key={i}>
-                  <ListItem>
-                    <Text as={"span"} fontWeight={"bold"}>
-                      Producto:
-                    </Text>{" "}
-                    {book.product}
-                  </ListItem>
-                  <ListItem>
-                    <Text as={"span"} fontWeight={"bold"}>
-                      Precio:
-                    </Text>{" "}
-                    {book.price}
-                  </ListItem>
-                </Box>
-              ))}
-            </ListItem>
-          </List>
-        </Box>
-        </Box>
->>>>>>> 013c696e6d0c08c79393b06a333c0b5bc42cce3b
       </Box>
+    </Box>
   );
 }
-
-
-              
-      
-     
