a56c4b5874566792dd288d5e7eed79905d3bef55
Armando Martinez <alemar.martinez16@gmail.com>
2023-07-18 04:13:52 -0300

    Se modifica estructura y distribucion de navbar

--- CLIENT/src/components/Login/Profile.jsx
+++ CLIENT/src/components/Login/Profile.jsx
@@ -1,13 +1,17 @@
-
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button, Image, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import {
+  Button,
+  Image,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
 import { FiLogOut } from "react-icons/fi";
-import styles from './Profile.module.css';
+import styles from "./Profile.module.css";
 import { clearStorageCart } from "../../Redux/Action/Index";
 import { useUserInfo } from "../../context/ProviderUser";
 import { Link } from "react-router-dom";
-
-
 
 export const Profile = () => {
   const { isAuthenticated, logout } = useAuth0();
@@ -20,17 +24,46 @@ export const Profile = () => {
   };
 
   return (
-    (isAuthenticated && user) && (
+    isAuthenticated &&
+    user && (
       <div className={styles.profile_zlksadaskj}>
         <Menu>
-          <MenuButton as={Button} variant="flat" colorScheme="gray" size="sm">
-            <Image src={user.picture} alt={`profile_${user.name}`} borderRadius="full" boxSize={8} objectFit="cover" />
-          </MenuButton>
+          <div
+            style={{
+              display: "inline-flex",
+              position: "relative",
+              borderRadius: "full",
+              overflow: "hidden",
+            }}
+            onClick={() => console.log("Imagen clickeada")} // Puedes agregar aquí la lógica que desees al hacer clic en la imagen
+          >
+            <Image
+              src={user.picture}
+              alt={`profile_${user.name}`}
+              borderRadius="full"
+              boxSize={12}
+              objectFit="cover"
+            />
+            <MenuButton
+              as={Button}
+              variant="flat"
+              colorScheme="green"
+              size="sm"
+              style={{
+                position: "absolute",
+                top: 0,
+                right: 0,
+                borderRadius: "full",
+                backgroundColor: "transparent",
+                boxShadow: "none",
+              }}
+            />
+          </div>
           <MenuList>
-
-            <MenuItem as={Link} to="/profile">Perfil</MenuItem>
-            <MenuItem onClick={handleLogout} icon={<FiLogOut />} command="⌘L">
-
+            <MenuItem as={Link} to="/profile">
+              Perfil
+            </MenuItem>
+            <MenuItem onClick={handleLogout} icon={<FiLogOut />}>
               Cerrar sesión
             </MenuItem>
           </MenuList>
@@ -40,3 +73,41 @@ export const Profile = () => {
   );
 };
 
+// import { useAuth0 } from "@auth0/auth0-react";
+// import { Button, Image, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+// import { FiLogOut } from "react-icons/fi";
+// import styles from './Profile.module.css';
+// import { clearStorageCart } from "../../Redux/Action/Index";
+// import { useUserInfo } from "../../context/ProviderUser";
+// import { Link } from "react-router-dom";
+
+// export const Profile = () => {
+//   const { isAuthenticated, logout } = useAuth0();
+//   const { user } = useUserInfo();
+
+//   const handleLogout = () => {
+//     clearStorageCart();
+//     // Hacer logout
+//     logout({ returnTo: window.location.origin });
+//   };
+
+//   return (
+//     (isAuthenticated && user) && (
+//       <div className={styles.profile_zlksadaskj}>
+//         <Menu>
+//           <MenuButton as={Button} variant="flat" colorScheme="gray" size="sm">
+//             <Image src={user.picture} alt={`profile_${user.name}`} borderRadius="full" boxSize={8} objectFit="cover" />
+//           </MenuButton>
+//           <MenuList>
+
+//             <MenuItem as={Link} to="/profile">Perfil</MenuItem>
+//             <MenuItem onClick={handleLogout} icon={<FiLogOut />} command="⌘L">
+
+//               Cerrar sesión
+//             </MenuItem>
+//           </MenuList>
+//         </Menu>
+//       </div>
+//     )
+//   );
+// };
