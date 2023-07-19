514e18a3e342ec4f7439f1caab6dd29f6fb11a84
maurocmg <maurocmg@gmail.com>
2023-07-19 17:37:06 -0300

    manejo de stock y sellings  funcionando

---
+++ API/src/controllers/checkStock.js
@@ -0,0 +1,17 @@
+const { Book } = require('../db');
+
+module.exports = async function({products}){
+  try {
+    for (let product of products) {
+      const { IdBook, quantity } = product;
+
+      const book = await Book.findByPk(IdBook);
+
+      if (quantity > book.stock) {
+        throw new Error(`Stock insuficiente del libro ${IdBook}`);
+      }
+    }
+  } catch (error) {
+    res.json({error: error.message});
+  }
+}
