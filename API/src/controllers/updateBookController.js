024f01fe9c06e522b22434a19ac475566216de16
daprionil <davidarenaoflol23@gmail.com>
2023-07-18 19:25:57 -0500

    Finalice route for Update Books

--- API/src/controllers/updateBookController.js
+++ API/src/controllers/updateBookController.js
@@ -1,3 +1,53 @@
-module.exports = async function(dataToUpdated){
+const { Book } = require("../db.js");
 
-}
+/* { price, stock, isActive} */
+
+const errorFieldsToUpdate = {
+    title: 'El titulo no es válido, debe tener entre 2 y 120 carácteres',
+    description: 'El titulo no es válido, debe tener entre 10 y 5000 carácteres',
+    image: 'La url de la imágen no es válida',
+    publisher: 'La editorial no es válida, debe contener entre 2 y 100 carácteres',
+    numberPages: 'La cantidad de páginas no es válida, no debe estar fuera del rango [1, 10000]',
+    price: 'El precio no es válido, no debe exceder los 1000.00 y tampoco ser menor a 0.00',
+    stock: 'El stock no es válido, debe ser mayor o igual a una cantidad de 0',
+    isActive: 'Debes enviar un valor Booleano para activo y/o desactivar un Libro'
+};
+
+const validationsAttributes = {
+    title: ({ length: l }) => l >= 2 && l <= 120,
+    description: ({ length: l }) => l >= 10 && l <= 5000,
+    image: imgUrl => /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm.test(imgUrl),
+    publisher: ({ length: l }) => l >= 2 && l <= 100,
+    numberPages: n => n >= 1 && n < 10000,
+    price: p => parseFloat(p) < 1000.00,
+    stock: n => n >= 0,
+    isActive: isA => ['true', 'false'].includes(isA.toString())
+};
+
+module.exports = async function (IdBook, updatedValues) {
+    //Validate query values
+    for (let [fieldKey, fieldValue] of Object.entries(updatedValues)) {
+        //Get function to field validate
+        const validationForField = validationsAttributes[fieldKey];
+        if (!validationForField) continue;
+
+        //Execute function to field validate
+        const validateValueField = validationForField(fieldValue);
+        if (!validateValueField) {
+            throw new Error(errorFieldsToUpdate[fieldKey]);
+        }
+    }
+
+    //At this point the values have no errors
+    const findBook = await Book.findOne({
+        where: { IdBook },
+    });
+
+    //Si el usuario no existe
+    if (!findBook) throw new Error(`El libro que intentas actualizar no existe`);
+
+    //Si ya existe lo actualiza
+    const book = await findBook.update(updatedValues);
+
+    return book;
+};
--- API/src/handlers/updateBookPutHandler.js
+++ API/src/handlers/updateBookPutHandler.js
@@ -1,9 +1,14 @@
+const { validate } = require("uuid");
 const updateBookController = require("../controllers/updateBookController");
 
 module.exports = async function(req,res){
     try {
         const {IdBook} = req.params;
         const { title, description, image, publisher, numberPages, price, stock, isActive} = req.body;
+        
+        //Validate if the Id is UUID
+        if(!validate(IdBook)) throw new Error("El Id no tiene un formato válido");
+        
         const objValues = { title, description, image, publisher, numberPages, price, stock, isActive};
         const validValues = Object.entries(objValues).filter(([k,v]) => {
             if(k === 'isActive') return v?.toString();
@@ -13,9 +18,9 @@ module.exports = async function(req,res){
         if(!validValues.length) throw new Error('No hay valores válidos para actualizar el libro');
         
         const objFinalValues = Object.fromEntries(validValues);
-        const UpdatedBook = await updateBookController(objFinalValues);
-        console.log(updateBookController);
-        res.send('eso');
+        const UpdatedBook = await updateBookController(IdBook, objFinalValues);
+        
+        res.json(UpdatedBook);
     } catch ({message}) {
         res.json({error:message});
     };
