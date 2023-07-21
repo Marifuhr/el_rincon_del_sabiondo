bcd2a0b3ca30baced3ad080c43258b35d40bcc73
Armando Martinez <alemar.martinez16@gmail.com>
2023-07-21 05:01:37 -0300

    Modicicacion del Carrito de compras

--- CLIENT/src/components/CartPayPage/CardBookCart.jsx
+++ CLIENT/src/components/CartPayPage/CardBookCart.jsx
@@ -1,11 +1,31 @@
-import { Box, Text, Image, Flex, Stack, Button } from '@chakra-ui/react'
-import React from 'react'
-import ButtonDeleteBookCart from '../ShoppingCart/ButtonDeleteBookCart';
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { increaseQuantity, decreaseQuantity } from '../../Redux/Action/Index';
+import {
+  Box,
+  Text,
+  Image,
+  Flex,
+  Stack,
+  Button,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Spacer,
+} from "@chakra-ui/react";
+import React from "react";
+import ButtonDeleteBookCart from "../ShoppingCart/ButtonDeleteBookCart";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { increaseQuantity, decreaseQuantity } from "../../Redux/Action/Index";
 
-const CardBookCart = ({ image, title, description, IdBook, price, quantity }) => {
+const CardBookCart = ({
+  image,
+  title,
+  description,
+  IdBook,
+  price,
+  quantity,
+}) => {
   const dispatch = useDispatch();
 
   const handleIncreaseQuantity = (id) => {
@@ -15,45 +35,262 @@ const CardBookCart = ({ image, title, description, IdBook, price, quantity }) =>
   const handleDecreaseQuantity = (id) => {
     dispatch(decreaseQuantity(id));
   };
-
-    return (
-        <Stack position="relative" maxH={{base:'100%', md:'300px'}} bg="rgba(0,0,0,0.1)" borderRadius={6} overflow="hidden" my={2}>
-            <Flex flexWrap={{base:'wrap',md:"nowrap"}} >
-                <Box maxHeight={{md:"sm", base:"200px"}} minH="100%" minWidth="100px" w="100%" overflow="hidden">
-                    <Image objectFit="cover" objectPosition="top" minH="100%" minW="100%" src={image} alt={title} />
-                </Box>
-                <Box p={4}>
-                    <Link to={`/detail/${IdBook}`}>
-                        <Text _hover={{color:"#163e1e", textDecoration:"underline"}} color="rgb(39, 39, 39)" fontWeight={600}>{title}</Text>
-                    </Link>
-                    <Text noOfLines={3}>{description}</Text>
-                    <Flex gap={{md:10, base:2}} flexWrap={{md:"nowrap", base:'wrap'}} alignItems="center">
-                        <Box bg="#70a57b" whiteSpace="nowrap" fontWeight="bold" borderRadius={3} px={4} py={1} boxShadow="0 1px 5px rgba(0,0,0,0.3)">
-                            <span>Precio Unitario</span>
-                            <span style={{color:'#163e1e'}}> ${price}</span>
-                        </Box>
-                        <Box bg="#70a57b" whiteSpace="nowrap" fontWeight="bold" borderRadius={3} px={4} py={1} boxShadow="0 1px 5px rgba(0,0,0,0.3)">
-                            <span>Cantidad Agregados: </span>
-                            <span style={{color:'#163e1e'}}>{quantity}</span>
-                        </Box>
-                    </Flex>
-                        <span>Total: </span>
-                        <span style={{color:'#ededed'}}>{Number(price * quantity).toFixed(2)}</span>
-                    </Box>
-                    <Box maxW="sm" my={2} py={1} width="min-content" bg="#70a57b" whiteSpace="nowrap" fontWeight="bold" borderRadius={3} px={4} boxShadow="0 1px 5px rgba(0,0,0,0.3)">
-                    <Box py={2}>
-                        <ButtonDeleteBookCart IdBook={IdBook} />
-                    </Box>
-                        <Box py={2}>
-         
-          <Button onClick={() => handleIncreaseQuantity(IdBook)}>+</Button>
-          <Button onClick={() => handleDecreaseQuantity(IdBook)}>-</Button>
+  return (
+    <Stack
+      position="relative"
+      maxH={{ base: "100%", sm: "300px" }}
+      bg="brand.primary"
+      boxShadow="0px 4px 10px rgba(0, 0, 0, 0.2)"
+      borderRadius={4}
+      overflow="hidden"
+      my={2}
+      p={2}
+    >
+      <Flex flexWrap={{ base: "wrap", md: "nowrap" }}>
+        <Box
+          maxHeight={{ md: "sm", base: "200px" }}
+          minH="100%"
+          minWidth={{ md: "200px", base: "100px" }}
+          w="100%"
+          overflow="hidden"
+          maxW={{ base: "100%", md: "150px" }}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Link to={`/detail/${IdBook}`}>
+            <Image
+              objectFit="cover"
+              objectPosition="top"
+              pr={{ md: 4, base: 1 }}
+              minH="10rem"
+              minW="8rem"
+              src={image}
+              alt={title}
+            />
+          </Link>
         </Box>
-                </Box>
-            </Flex>
-            
-        </Stack>
-    )
-}
 
-export default CardBookCart
+        {/* TITULO, DESCRIPCION, PRECIO, CANTIDAD, TOTAL */}
+        <Box p={2} maxW={{ base: "100%", md: "sm" }}>
+          <Link to={`/detail/${IdBook}`} noUnderline={true}>
+            <Text
+              fontSize={{
+                base: "1xl",
+                md: "1xl",
+                lg: "2xl",
+              }}
+              color="rgb(39, 39, 39)"
+              fontWeight={600}
+              lineHeight="shorter"
+              textShadow="2px 2px 3px rgba(0, 0, 0, 0.5)"
+              textAlign={{ base: "center", md: "left" }}
+            >
+              {title}
+            </Text>
+          </Link>
+          <Text
+            noOfLines={2}
+            fontSize="12px"
+            textAlign={{ base: "center", md: "justify" }}
+            lineHeight="1.2em"
+          >
+            {description}
+          </Text>
+          <Flex
+            gap={{ md: 10, base: 2 }}
+            flexWrap={{ md: "nowrap", base: "wrap" }}
+            alignItems={{ md: "center", base: "flex-end" }}
+            flexDirection={{ md: "row", base: "column" }}
+          >
+            <Box
+              display="flex"
+              justifyContent="flex-end"
+              margin={{ base: "auto", md: 0 }}
+            >
+              <ButtonDeleteBookCart IdBook={IdBook} />
+            </Box>
+          </Flex>
+        </Box>
+
+        <Box
+          maxW="200px"
+          width="100%"
+          bg="#70a57b"
+          whiteSpace="nowrap"
+          fontWeight="bold"
+          borderRadius={3}
+          boxShadow="0 1px 5px rgba(0,0,0,0.3)"
+          mx={{ base: "auto", md: 0 }}
+        >
+          <Box
+            width="100%"
+            maxWidth={{ md: "unset", base: "550px" }}
+            px={{ md: 4, base: 2 }}
+            py={2}
+          >
+            <Stack direction="column" spacing={2}>
+              {/* Cantidad */}
+              <Stack
+                shouldWrapChildren
+                direction="row"
+                alignItems="center"
+                isTruncated
+              >
+                <Flex>
+                  <span>Cantidad</span>
+                </Flex>
+                <Flex>
+                  <NumberInput
+                    size="xs"
+                    maxW={12}
+                    defaultValue={quantity}
+                    min={1}
+                  >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper
+                        onClick={() => handleIncreaseQuantity(IdBook)}
+                      />
+                      <NumberDecrementStepper
+                        onClick={() => handleDecreaseQuantity(IdBook)}
+                      />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </Flex>
+              </Stack>
+              {/* Precio Unitario */}
+              <Box
+                bg="#70a57b"
+                whiteSpace="nowrap"
+                fontWeight="bold"
+                borderRadius={3}
+                pt={"2px"}
+                pr={2}
+                boxShadow="0 1px 5px rgba(0,0,0,0.3)"
+                fontSize="12px"
+              >
+                <Stack direction="column" spacing={0}>
+                  <span
+                    style={{
+                      fontSize: "16px",
+                      color: "blue",
+                      fontWeight: "bold",
+                      textAlign: "right",
+                    }}
+                  >
+                    ${price}
+                  </span>
+                  <span
+                    style={{
+                      textAlign: "left",
+                      color: "black",
+                      padding: "5px",
+                    }}
+                  >
+                    Precio Unitario:
+                  </span>
+                </Stack>
+              </Box>
+              {/* Total */}
+              <Box
+                bg="#70a57b"
+                whiteSpace="nowrap"
+                fontWeight="bold"
+                borderRadius={3}
+                px={2}
+                py={3}
+                boxShadow="0 1px 5px rgba(0,0,0,0.3)"
+                fontSize="16px"
+              >
+                <Stack direction="column" spacing={0}>
+                  <span
+                    style={{
+                      alignSelf: "flex-start",
+                      textShadow: "0 1px 5px rgba(0,0,0,0.3)",
+                    }}
+                  >
+                    Total:
+                  </span>
+                  <span
+                    style={{
+                      fontSize: "22px",
+                      color: "white",
+                      fontWeight: "bold",
+                      textAlign: "center",
+                      textShadow: "0 1px 5px rgba(0,0,0,0.3)",
+                    }}
+                  >
+                    ${Number(price * quantity).toFixed(2)}
+                  </span>
+                </Stack>
+              </Box>
+            </Stack>
+          </Box>
+        </Box>
+      </Flex>
+    </Stack>
+  );
+};
+
+export default CardBookCart;
+
+// import { Box, Text, Image, Flex, Stack, Button } from '@chakra-ui/react'
+// import React from 'react'
+// import ButtonDeleteBookCart from '../ShoppingCart/ButtonDeleteBookCart';
+// import { Link } from 'react-router-dom';
+// import { useDispatch } from 'react-redux';
+// import { increaseQuantity, decreaseQuantity } from '../../Redux/Action/Index';
+
+// const CardBookCart = ({ image, title, description, IdBook, price, quantity }) => {
+//   const dispatch = useDispatch();
+
+//   const handleIncreaseQuantity = (id) => {
+//     dispatch(increaseQuantity(id));
+//   };
+
+//   const handleDecreaseQuantity = (id) => {
+//     dispatch(decreaseQuantity(id));
+//   };
+
+//     return (
+//         <Stack position="relative" maxH={{base:'100%', md:'300px'}} bg="rgba(0,0,0,0.1)" borderRadius={6} overflow="hidden" my={2}>
+//             <Flex flexWrap={{base:'wrap',md:"nowrap"}} >
+//                 <Box maxHeight={{md:"sm", base:"200px"}} minH="100%" minWidth="100px" w="100%" overflow="hidden">
+//                     <Image objectFit="cover" objectPosition="top" minH="100%" minW="100%" src={image} alt={title} />
+//                 </Box>
+//                 <Box p={4}>
+//                     <Link to={`/detail/${IdBook}`}>
+//                         <Text _hover={{color:"#163e1e", textDecoration:"underline"}} color="rgb(39, 39, 39)" fontWeight={600}>{title}</Text>
+//                     </Link>
+//                     <Text noOfLines={3}>{description}</Text>
+//                     <Flex gap={{md:10, base:2}} flexWrap={{md:"nowrap", base:'wrap'}} alignItems="center">
+//                         <Box bg="#70a57b" whiteSpace="nowrap" fontWeight="bold" borderRadius={3} px={4} py={1} boxShadow="0 1px 5px rgba(0,0,0,0.3)">
+//                             <span>Precio Unitario</span>
+//                             <span style={{color:'#163e1e'}}> ${price}</span>
+//                         </Box>
+//                         <Box bg="#70a57b" whiteSpace="nowrap" fontWeight="bold" borderRadius={3} px={4} py={1} boxShadow="0 1px 5px rgba(0,0,0,0.3)">
+//                             <span>Cantidad Agregados: </span>
+//                             <span style={{color:'#163e1e'}}>{quantity}</span>
+//                         </Box>
+//                     </Flex>
+//                         <span>Total: </span>
+//                         <span style={{color:'#ededed'}}>{Number(price * quantity).toFixed(2)}</span>
+//                     </Box>
+//                     <Box maxW="sm" my={2} py={1} width="min-content" bg="#70a57b" whiteSpace="nowrap" fontWeight="bold" borderRadius={3} px={4} boxShadow="0 1px 5px rgba(0,0,0,0.3)">
+//                     <Box py={2}>
+//                         <ButtonDeleteBookCart IdBook={IdBook} />
+//                     </Box>
+//                         <Box py={2}>
+
+//           <Button onClick={() => handleIncreaseQuantity(IdBook)}>+</Button>
+//           <Button onClick={() => handleDecreaseQuantity(IdBook)}>-</Button>
+//         </Box>
+//                 </Box>
+//             </Flex>
+
+//         </Stack>
+//     )
+// }
+
+// export default CardBookCart
--- CLIENT/src/components/CartPayPage/CartPayPage.jsx
+++ CLIENT/src/components/CartPayPage/CartPayPage.jsx
@@ -1,56 +1,290 @@
-import { Box, Text, Heading, Stack, Button } from "@chakra-ui/react";
-import { useSelector } from "react-redux"
-import Footer from '../Footer/Footer';
-import ButtonVolver from '../../elements/ButtonVolver';
+import {
+  Box,
+  Text,
+  Heading,
+  Flex,
+  Stack,
+  Button,
+  Image,
+} from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import Footer from "../Footer/Footer";
+import ButtonVolver from "../../elements/ButtonVolver";
 import CardBookCart from "./CardBookCart";
 import WalletMercadoPago from "../WalletMercadoPago/WalletMercadoPago";
 import { Link } from "react-router-dom";
+import Carousel from "../Carousel/Carousel";
+import imageBannerOferta from "../../assets/Libros_imagenes/bannerOferta1.1.jpg";
 
 const CartPayPage = () => {
   const [storageCartShopping, count] = useSelector(({ cart_shopping }) => {
     const count = cart_shopping.reduce((init, { price, quantity }) => {
-      return init + (price * quantity);
+      return init + price * quantity;
     }, 0);
 
-    return [cart_shopping, count.toFixed(2)]
+    return [cart_shopping, count.toFixed(2)];
   });
 
   return (
-    <Stack p={2}>
+    <Flex flexDirection="column" minHeight="100vh">
       <Box>
         <ButtonVolver />
-        <Heading p={2} textAlign={"center"} fontSize="4xl">Mi Carrito de Compras</Heading>
+        <Heading
+          pb={10}
+          mb={0}
+          pt={0}
+          textAlign={"center"}
+          fontSize={{
+            base: "4xl",
+            md: "4xl",
+            lg: "5xl",
+          }}
+          fontWeight="bold"
+          color="brand.tertiary"
+          _dark={{
+            color: "gray.300",
+          }}
+          lineHeight="shorter"
+          textShadow="2px 2px 3px rgba(0, 0, 0, 0.5)"
+        >
+          Mi Carrito de Compras
+        </Heading>
       </Box>
-      {
-        storageCartShopping.length ?
-          <>
-            <Box bg="#009a3619" maxW='4xl' w="100%" m={'0 auto'} p={2}>
-              {
-                storageCartShopping.map((book) => (
-                  <CardBookCart key={book.IdBook} {...book} />
-                ))
-              }
-            </Box>
-            <Box maxW="sm" mx="auto">
-              <Text fontSize="2xl" p={2} borderRadius={4} boxShadow="0 1px 5px rgba(0,0,0,0.3)">
-                <span>Total Compra:</span>
-                <span style={{ fontWeight: "600" }}> ${count}</span>
-              </Text>
-              <WalletMercadoPago products={storageCartShopping} />
-            </Box>
-          </>
-          : <>
-              <Box maxW="xl" mx="auto" textAlign="center">
-                <Text fontSize="2xl">Aún no tienes Elementos en el Carrito</Text>
-                <Link to="/home">
-                  <Button bg="#315238" color="white">Agrega Uno</Button>
-                </Link>
-              </Box>
-            </>
-      }
-      <Footer />
-    </Stack>
-  )
-}
 
-export default CartPayPage
+      <Flex
+        flex={1}
+        flexDirection={{ base: "column", md: "row" }}
+        mb={5}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          flex={4}
+          ml={10}
+          mr={10}
+          overflowY="auto"
+          bg="brand.primary"
+          boxShadow="0 2px 4px rgba(0,0,0,0.1)"
+          borderRadius="4px"
+          padding="10px"
+          maxHeight="calc(107vh - 200px)"
+          maxWidth={820}
+          css={{
+            "&::-webkit-scrollbar": {
+              width: "8px",
+            },
+            "&::-webkit-scrollbar-track": {
+              backgroundColor: "#a3c5aa",
+              borderRadius: "5px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#448f53",
+              borderRadius: "5px",
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
+              backgroundColor: "#448f53",
+            },
+          }}
+        >
+          {storageCartShopping.length ? (
+            <Stack spacing={4}>
+              {storageCartShopping.map((book) => (
+                <CardBookCart key={book.IdBook} {...book} />
+              ))}
+            </Stack>
+          ) : (
+            <Box maxW="xl" mx="auto" textAlign="center">
+              <Text fontSize="2xl">Aún no tienes Elementos en el Carrito</Text>
+              <Link to="/home">
+                <Button bg="#315238" color="white">
+                  Agrega Uno
+                </Button>
+              </Link>
+            </Box>
+          )}
+        </Box>
+        <Flex
+          flex={1}
+          flexDirection={{ base: "column", md: "column" }}
+          mb={5}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box flex={1} maxW="sm" mx={15} pt={10}>
+            <Text
+              fontSize="2xl"
+              p={2}
+              borderRadius={4}
+              boxShadow="0 1px 5px rgba(0,0,0,0.3)"
+            >
+              <span>Total Compra:</span>
+              <span
+                style={{
+                  fontWeight: "700",
+                  color: "#315238",
+                  position: "relative",
+                  top: "3px",
+                  display: "inline-block",
+                  textAlign: "right",
+                  marginLeft: "5px",
+                }}
+              >
+                ${count}
+              </span>
+            </Text>
+            <WalletMercadoPago products={storageCartShopping} />
+          </Box>
+          <Link to={`/detail/f9573dec-615f-4fb8-97d0-68f1589b5934`}>
+            <Image
+              src={imageBannerOferta}
+              alt="cart"
+              objectFit="cover"
+              mt={5}
+              boxShadow="0px 5px 15px rgba(0, 0, 0, 0.2)"
+              transition="transform 0.2s ease-in-out"
+              _hover={{
+                transform: "translateY(-3px) scale(0.85)",
+              }}
+              transform="scale(0.8)"
+            />
+          </Link>
+        </Flex>
+      </Flex>
+
+      <Carousel />
+      <Footer />
+    </Flex>
+  );
+};
+
+export default CartPayPage;
+
+// import { Box, Text, Heading, Stack, Button } from "@chakra-ui/react";
+// import { useSelector } from "react-redux"
+// import Footer from '../Footer/Footer';
+// import ButtonVolver from '../../elements/ButtonVolver';
+// import CardBookCart from "./CardBookCart";
+// import WalletMercadoPago from "../WalletMercadoPago/WalletMercadoPago";
+// import { Link } from "react-router-dom";
+
+// const CartPayPage = () => {
+//   const [storageCartShopping, count] = useSelector(({ cart_shopping }) => {
+//     const count = cart_shopping.reduce((init, { price, quantity }) => {
+//       return init + (price * quantity);
+//     }, 0);
+
+//     return [cart_shopping, count.toFixed(2)]
+//   });
+
+//   return (
+//     <Stack p={2}>
+//       <Box>
+//         <ButtonVolver />
+//         <Heading p={2} textAlign={"center"} fontSize="4xl">Mi Carrito de Compras</Heading>
+//       </Box>
+//       {
+//         storageCartShopping.length ?
+//           <>
+//             <Box bg="#009a3619" maxW='4xl' w="100%" m={'0 auto'} p={2}>
+//               {
+//                 storageCartShopping.map((book) => (
+//                   <CardBookCart key={book.IdBook} {...book} />
+//                 ))
+//               }
+//             </Box>
+//             <Box maxW="sm" mx="auto">
+//               <Text fontSize="2xl" p={2} borderRadius={4} boxShadow="0 1px 5px rgba(0,0,0,0.3)">
+//                 <span>Total Compra:</span>
+//                 <span style={{ fontWeight: "600" }}> ${count}</span>
+//               </Text>
+//               <WalletMercadoPago products={storageCartShopping} />
+//             </Box>
+//           </>
+//           : <>
+//               <Box maxW="xl" mx="auto" textAlign="center">
+//                 <Text fontSize="2xl">Aún no tienes Elementos en el Carrito</Text>
+//                 <Link to="/home">
+//                   <Button bg="#315238" color="white">Agrega Uno</Button>
+//                 </Link>
+//               </Box>
+//             </>
+//       }
+//       <Footer />
+//     </Stack>
+//   )
+// }
+
+// export default CartPayPage
+
+{
+  /* <Flex flex={1} mb={5}>
+        <Box
+          flex={4}
+          ml={10}
+          mr={10}
+          overflowY="auto"
+          bg="brand.tertiary"
+          boxShadow="0 2px 4px rgba(0,0,0,0.1)"
+          borderRadius="4px"
+          padding="10px"
+          maxHeight="calc(108vh - 200px)"
+          css={{
+            "&::-webkit-scrollbar": {
+              width: "8px",
+            },
+            "&::-webkit-scrollbar-track": {
+              backgroundColor: "#a3c5aa",
+              borderRadius: "5px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#448f53",
+              borderRadius: "5px",
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
+              backgroundColor: "#448f53",
+            },
+          }}
+        >
+          {storageCartShopping.length ? (
+            <Stack spacing={4}>
+              {storageCartShopping.map((book) => (
+                <CardBookCart key={book.IdBook} {...book} />
+              ))}
+            </Stack>
+          ) : (
+            <Box maxW="xl" mx="auto" textAlign="center">
+              <Text fontSize="2xl">Aún no tienes Elementos en el Carrito</Text>
+              <Link to="/home">
+                <Button bg="#315238" color="white">
+                  Agrega Uno
+                </Button>
+              </Link>
+            </Box>
+          )}
+        </Box>
+        <Box flex={1} maxW="sm" mx={15} pt={40}>
+          <Text
+            fontSize="2xl"
+            p={2}
+            borderRadius={4}
+            boxShadow="0 1px 5px rgba(0,0,0,0.3)"
+          >
+            <span>Total Compra:</span>
+            <span
+              style={{
+                fontWeight: "700",
+                color: "#315238",
+                position: "relative",
+                top: "3px",
+                display: "inline-block",
+                textAlign: "right",
+                marginLeft: "5px",
+              }}
+            >
+              ${count}
+            </span>
+          </Text>
+          <WalletMercadoPago products={storageCartShopping} />
+        </Box>
+      </Flex> */
+}
